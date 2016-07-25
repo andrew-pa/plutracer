1f97a1fc214a0bec195b22a6868e5310d54c05a2
Andrew <andrew.pa@outlook.com>
2016-07-25 06:34:38 -0700

    Initial Commit! - created CMake file + main.cpp

---
+++ inc/cmmn.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <exception>
+#include <string>
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <map>
+#include <functional>
+#include <algorithm>
+using namespace std;
+
+#define GLM_SWIZZLE
+#include <glm/glm.hpp>
+using namespace glm;
---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+
+#include "cmmn.h"
+
+int main(int argc, char* argv[]) {
+	vector<string> args; for (int i = 1; i < argc; ++i) args.push_back(argv[i]);
+
+	return 0;
+}
