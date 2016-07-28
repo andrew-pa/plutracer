22144d7b14e4b82d11d9771bb015aa8403907c5b
Arman Talkar <ajtalkar@live.com>
2016-07-27 19:39:24 -0700

    bvh_node not subclass of aabb; add 'object' field to bvh_node

--- inc/bvh_node.h
+++ inc/bvh_node.h
@@ -2,30 +2,38 @@
 
 #include <list>
 #include "cmmn.h"
+#include "surfaces\surfaces.h"
 
 
  namespace plu {
 
- 	struct bvh_node : public aabb {
+ 	struct bvh_node {
  	public:
- 		bvh_node * parent; bvh_node * left_child; bvh_node * right_child;
+ 		bvh_node * parent;
+ 		bvh_node * left_child;
+ 		bvh_node * right_child;
 
- 		bvh_node()
- 		: aabb() {
- 			parent = nullptr;
- 			left_child = nullptr;
- 			right_child = nullptr;
+ 		aabb bounds;
+
+ 		surface * object;
+
+ 		bvh_node() 
+ 		: parent(nullptr), left_child(nullptr), right_child(nullptr), object(nullptr) {
+ 			bounds = aabb();
  		}
 
  		bvh_node(bvh_node * p, bvh_node * lc, bvh_node * rc, vec3 mi, vec3 mx)
- 		: aabb(mi, mx) {
- 			parent = p;
- 			left_child = lc;
- 			right_child = rc;
+ 		: parent(p), left_child(lc), right_child(rc), object(nullptr) {
+ 			bounds = aabb(mi, mx);
+ 		}
+
+ 		bvh_node(bvh_node * p, surface * o) 
+ 		: parent(p), left_child(nullptr), right_child(nullptr), object(o) {
+ 			bounds = object->bounds();
  		}
 
  		bool is_leaf() {
- 			return (left_child == nullptr && right_child == nullptr);
+ 			return (object != nullptr);
  		}
  	}
  }
