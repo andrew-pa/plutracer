df6e7c78e862306c5cdf07de5c8abce36902bdb5
Andrew <andrew.pa@outlook.com>
2016-08-02 15:20:19 -0700

    added evaluation to scene loader, new scene to test such

--- src/main.cpp
+++ src/main.cpp
@@ -20,13 +20,15 @@
 !!	
 !!	Where to eval? who knows.... this needs major help
 !!
-!!	RIGHT NOW THE SCRIPTING/EXECUTION PORTION OF URN IS UNUSED
+!!	RIGHT NOW THE SCRIPTING/EXECUTION PORTION OF URN IS USED, BUT NOT EVERYWHERE
 !!	This is because
 !!	1) I don't know what to eval and what not to, 
 !!		it can't yet be deterimined where it will be useful
 !!	2) Namespacing issues related to what gets eval'd and in what eval_context
 !!	3) urn has no std lib, not even basic functions like + or - or even a map, 
 !!		so it is basically useless. Someone needs to go and define those
+!!	RIGHT NOW: Scene loader evaluates basically everything in the same evaluation context
+!!		Additionally it evaluates once the entire object value
 !!
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
@@ -67,6 +69,11 @@ inline vec3 bk2v3(const urn::value& v) {
 	return vec3(v[0].get_num(), v[1].get_num(), v[2].get_num());
 }
 
+inline vec3 bk2v3(urn::eval_context& cx, const urn::value& v) {
+	auto rv = cx.reduce(v);
+	return vec3(rv[0].get_num(), rv[1].get_num(), rv[2].get_num());
+}
+
 /*
 Example of a scene definition:
 
@@ -92,7 +99,7 @@ objects: do [
 ]
 */
 
-inline shared_ptr<plu::material> make_material(const urn::value& v) {
+inline shared_ptr<plu::material> make_material(urn::eval_context& cx, const urn::value& v) {
 	auto vs = v.get<vector<urn::value>>();
 	if (vs[0].get_var() == "diffuse") {
 		plu::props::color c = plu::props::color(vec3(0.f));
@@ -102,26 +109,30 @@ inline shared_ptr<plu::material> make_material(const urn::value& v) {
 				auto ts = vs[2].get<vector<urn::value>>();
 				auto t = ts[0].get_var();
 				if (t == "checkerboard") {
-					c = plu::props::color(make_shared<plu::textures::checkerboard_texture>(bk2v3(ts[1]), bk2v3(ts[2]), ts[3].get_num()));
+					c = plu::props::color(
+						make_shared<plu::textures::checkerboard_texture>(bk2v3(cx, ts[1]), bk2v3(cx, ts[2]), cx.eval(ts[3]).get_num()));
 				}
 				else if (t == "grid") {
-					c = plu::props::color(make_shared<plu::textures::grid_texture>(bk2v3(ts[1]), bk2v3(ts[2]), ts[3].get_num(), ts[4].get<double>()));
+					c = plu::props::color(
+						make_shared<plu::textures::grid_texture>(bk2v3(cx, ts[1]), bk2v3(cx, ts[2]), cx.eval(ts[3]).get_num(), cx.eval(ts[4]).get<double>()));
 				}
 				else if (t == "img") {
-					c = plu::props::color(make_shared<plu::texture2d>(ts[1].get<string>()));
+					c = plu::props::color(
+						make_shared<plu::texture2d>(ts[1].get<string>()));
 				}
 				else throw;
 
 			} else throw;
 		}
 		else if (vs[1].type == urn::value::Block) {
-			c = plu::props::color(bk2v3(vs[1]));
+			c = plu::props::color(bk2v3(cx, vs[1]));
 		}
 		else throw;
 		return make_shared<plu::material>(c);
 	}
 	else throw;
 }
+
 
 int main(int argc, char* argv[]) {
 	vector<string> args; for (int i = 1; i < argc; ++i) args.push_back(argv[i]);
@@ -130,10 +141,31 @@ int main(int argc, char* argv[]) {
 	// !! also where else would it go anyways?
 
 	// ***** this is still preliminary, and will probably change significatly *****
+	int argi = 0;
+
+	// a little silly REPL loop so that it's easier to test urn
+	// perhaps one day this will happen after it loads the scene so that you can modify that
+	if(args[argi] == "/i") {
+		argi++;
+		urn::eval_context cx;
+		cx.create_std_funcs();
+		while (true) {
+			string s;
+			cout << "urn> ";
+			getline(cin, s);
+			auto v = urn::value(urn::token_stream(istringstream(s)));
+			if (v.type == urn::value::Val) {
+				auto cmd = v.get_val();
+				if (cmd == "!q") break;
+				else if (cmd == "!x") return 42;
+			}
+			cout << cx.eval(v) << endl;
+		}
+	}
 
 	auto init_start = chrono::high_resolution_clock::now();
 
-	urn::value scene_tlv = urn::value(urn::token_stream(ifstream(args[0]))); //requires first cmdline argument to be path to scene file for now
+	urn::value scene_tlv = urn::value(urn::token_stream(ifstream(args[argi]))); //requires first cmdline argument to be path to scene file for now
 
 	auto resolu_b = scene_tlv.named_block_val("resolution");
 	auto resolution = resolu_b.is_null() ? uvec2(1280, 960) : uvec2(resolu_b[0].get<int64_t>(), resolu_b[1].get<int64_t>());
@@ -147,26 +179,31 @@ int main(int argc, char* argv[]) {
 	map<string, shared_ptr<plu::material>> mats;
 	vector<shared_ptr<plu::surface>> surfs;
 
+
+	urn::eval_context cx;
+	cx.create_std_funcs();
+
 	auto matvs = scene_tlv.named_block_val("materials").get<vector<urn::value>>();
 	for (int i = 0; i < matvs.size(); ++i) {
 		if (matvs[i].type != urn::value::Def) throw;
 		auto d = matvs[i].get<pair<string, urn::value>>();
-		mats[d.first] = make_material(d.second);
+		mats[d.first] = make_material(cx, d.second);
 	}
 
-	auto objvs = scene_tlv.named_block_val("objects").get<vector<urn::value>>();
+
+	auto objvs = cx.eval1(scene_tlv.named_block_val("objects")).get<vector<urn::value>>();
 	for (int i = 0; i < objvs.size();) {
 		auto ot = objvs[i].get_var();
 		if (ot == "sphere") {
 			auto m = objvs[i + 3];
-			auto M = m.type == urn::value::Block ? make_material(m) : m.type == urn::value::Id ? mats[m.get_id()] : nullptr;
-			surfs.push_back(make_shared<plu::surfaces::sphere>(bk2v3(objvs[i+1]), objvs[i+2].get_num(), M));
+			auto M = m.type == urn::value::Block ? make_material(cx, m) : m.type == urn::value::Id ? mats[m.get_id()] : nullptr;
+			surfs.push_back(make_shared<plu::surfaces::sphere>(bk2v3(cx, objvs[i+1]), cx.eval(objvs[i+2]).get_num(), M));
 			i += 4;
 		}
 		else if (ot == "box") {
 			auto m = objvs[i + 3];
-			auto M = m.type == urn::value::Block ? make_material(m) : m.type == urn::value::Id ? mats[m.get_id()] : nullptr;
-			surfs.push_back(make_shared<plu::surfaces::box>(bk2v3(objvs[i + 1]), bk2v3(objvs[i + 2]), M));
+			auto M = m.type == urn::value::Block ? make_material(cx, m) : m.type == urn::value::Id ? mats[m.get_id()] : nullptr;
+			surfs.push_back(make_shared<plu::surfaces::box>(bk2v3(cx, objvs[i + 1]), bk2v3(cx, objvs[i + 2]), M));
 			i += 4;
 		}
 	}
